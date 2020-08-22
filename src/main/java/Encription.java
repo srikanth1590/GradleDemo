06af89a37ea55a2eb4e0a12449028b9079195bf3
Srikanth Takkellapati <srikanth1590@gmail.com>
2020-08-22 12:21:22 +1000

    removing intentional failures, brought back to original code

--- src/main/java/Encription.java
+++ src/main/java/Encription.java
@@ -1,7 +1,6 @@
 public class Encription {
 
-    //final char[] a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    final char[] a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'a', 'i', 'j', 'a', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 'a', 't', 'u', 'v', 'w', 'x', 'y', 'a'};
+    final char[] a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
     public static void main(String[] args) {
 
