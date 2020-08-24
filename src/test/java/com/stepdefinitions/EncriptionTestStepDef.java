203ab48ab1a49d2e27fdb2577847337a1c743012
Srikanth Takkellapati <srikanth1590@gmail.com>
2020-08-24 22:10:00 +1000

    Cucumber implementation  - Added all scenarios

--- src/test/java/com/stepdefinitions/EncriptionTestStepDef.java
+++ src/test/java/com/stepdefinitions/EncriptionTestStepDef.java
@@ -5,29 +5,26 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-
 import static org.junit.Assert.assertEquals;
 
 public class EncriptionTestStepDef {
 
     Encription e = new Encription();
+    String result = null;
 
-    @Given("I got my application")
-    public void i_got_my_application() {
-        assertEquals("ssk76vu", e.performEncription("abc76dd", "sri4"));
+    @Given("That I'm testing {string} for Encryption application")
+    public void that_I_m_testing_for_Encryption_application(String string) {
+        System.out.println("Testing scenario : " + string);
     }
 
-    @When("I run the method")
-    public void i_run_the_method() {
-        System.out.println("When");
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
+    @When("I perform encryption for the given {string} and {string} combination")
+    public void i_perform_encryption_for_the_given_and_combination(String string, String string2) {
+        result = e.performEncription(string, string2);
     }
 
-    @Then("I should be able to see the results")
-    public void i_should_be_able_to_see_the_results() {
-        System.out.println("Then");
-        // Write code here that turns the phrase above into concrete actions
-        //throw new io.cucumber.java.PendingException();
+    @Then("I should get the correct {string} for the given sentence.")
+    public void i_should_get_the_correct_for_the_given_sentence(String string) {
+        assertEquals(string, result);
     }
+
 }
